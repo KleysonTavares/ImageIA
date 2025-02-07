945f6d25acc58fb8f054be9c3bd88a04e54818c4
Kleyson Tavares <kleyson.tavares@icloud.com>
2025-02-07 20:24:47 -0300

    Adiciona plugin para ImagineArt

--- ImageIA/Controller/ViewController.swift
+++ ImageIA/Controller/ViewController.swift
@@ -10,14 +10,61 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var descriptionImage: UITextView!
-
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBAction func searchImageButton(_ sender: UIButton) {
+        activityIndicator.isHidden = false
+        searchImagineArt()
+    }
+    
+    let serviceDall_e = ServiceDall_e()
+    let serviceImagineArt = ServiceImagineArt()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        activityIndicator.isHidden = true
+    }
+    
+    func searchImagineArt() {
         if let savedUrl = UserDefaults.standard.string(forKey: "savedImageURL"), let url = URL(string: savedUrl) {
             loadImage(from: url) // Carrega a imagem do cache
         } else {
-            generateImage(prompt: descriptionImage.text) { imageUrl in
+            serviceImagineArt.generateImage(prompt: descriptionImage.text) { data in
+                DispatchQueue.main.async {
+                    if let data = data {
+                        self.loadImageImagineArt(from: data)
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    func loadImageImagineArt(from data: Data) {
+        DispatchQueue.main.async {
+            self.activityIndicator.startAnimating() // Inicia o Spinner
+        }
+        
+        DispatchQueue.global().async {
+            if let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                    self.endLoading()
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.endLoading()
+                    self.showErrorAlert(message: "Não foi possível gerar a imagem. Tente novamente mais tarde.")
+                }
+            }
+        }
+    }
+    
+    
+    func searchImageDall_e() {
+        if let savedUrl = UserDefaults.standard.string(forKey: "savedImageURL"), let url = URL(string: savedUrl) {
+            loadImage(from: url) // Carrega a imagem do cache
+        } else {
+            serviceDall_e.generateImage(prompt: descriptionImage.text) { imageUrl in
                 DispatchQueue.main.async {
                     if let imageUrl = imageUrl, let url = URL(string: imageUrl) {
                         self.loadImage(from: url)
@@ -29,10 +76,20 @@ class ViewController: UIViewController {
     }
     
     func loadImage(from url: URL) {
+        DispatchQueue.main.async {
+            self.startLoading()
+        }
+        
         DispatchQueue.global().async {
             if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
                 DispatchQueue.main.async {
                     self.imageView.image = image
+                    self.endLoading()
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.endLoading()
+                    self.showErrorAlert(message: "Não foi possível gerar a imagem. Tente novamente mais tarde.")
                 }
             }
         }
@@ -59,4 +116,22 @@ class ViewController: UIViewController {
         return nil
     }
     
+    func showErrorAlert(message: String) {
+        let alert = UIAlertController(title: "Erro", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        DispatchQueue.main.async {
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func startLoading() {
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating() // Inicia o Spinner
+    }
+    
+    func endLoading() {
+        activityIndicator.isHidden = true
+        activityIndicator.stopAnimating() // Finaliza o Spinner
+    }
 }
---
+++ ImageIA/ServiceImagineArt/serviceImagineArt.swift
@@ -0,0 +1,98 @@
+//
+//  serviceImagineArt.swift
+//  ImageIA
+//
+//  Created by Kleyson Tavares on 07/02/25.
+//
+
+import UIKit
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
+class ServiceImagineArt {
+    var semaphore = DispatchSemaphore (value: 0)
+    var body = ""
+    var error: Error? = nil
+    let boundary = "Boundary-\(UUID().uuidString)"
+    let parameters = [
+        [
+            "key": "prompt",
+            "value": "A futuristic cityscape at night with neon lights",
+            "type": "text"
+        ],
+        [
+            "key": "style",
+            "value": "realistic",
+            "type": "text"
+        ],
+        [
+            "key": "aspect_ratio",
+            "value": "1:1",
+            "type": "text"
+        ],
+        [
+            "key": "seed",
+            "value": "5",
+            "type": "text"
+        ]] as [[String : Any]]
+    
+    
+    func generateImage(prompt: String, completion: @escaping (Data?) -> Void) {
+        for param in parameters {
+            if param["disabled"] == nil {
+                let paramName = param["key"]!
+                body += "--\(boundary)\r\n"
+                body += "Content-Disposition:form-data; name=\"\(paramName)\""
+                if param["contentType"] != nil {
+                    body += "\r\nContent-Type: \(param["contentType"] as! String)"
+                }
+                let paramType = param["type"] as! String
+                if paramType == "text" {
+                    let paramValue = param["value"] as! String
+                    body += "\r\n\r\n\(paramValue)\r\n"
+                } else {
+                    let paramSrc = param["src"] as! String
+                    do {
+                        let fileData = try NSData(contentsOfFile:paramSrc, options:[]) as Data
+                        let fileContent = String(data: fileData, encoding: .utf8)!
+                        body += "; filename=\"\(paramSrc)\"\r\n"
+                        + "Content-Type: \"content-type header\"\r\n\r\n\(fileContent)\r\n"
+                    } catch {
+                        print("erro")
+                    }
+                }
+            }
+        }
+        
+        
+        body += "--\(boundary)--\r\n";
+        let postData = body.data(using: .utf8)
+        
+        var request = URLRequest(url: URL(string: "https://api.vyro.ai/v2/image/generations")!,timeoutInterval: Double.infinity)
+        request.addValue("Bearer vk-92EXelKV38TeZWEsjBKvEBlEMDhwG3ZgpWgoHcJ2fWr3xD0", forHTTPHeaderField: "Authorization")
+        request.addValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        request.httpMethod = "POST"
+        request.httpBody = postData
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print(String(describing: error))
+                self.semaphore.signal()
+                return
+            }
+            self.semaphore.signal()
+            
+            do {
+                    completion(data)
+            } catch {
+                print("Erro ao decodificar resposta:", error.localizedDescription)
+                completion(nil)
+            }
+        }
+        
+        task.resume()
+        semaphore.wait()
+    }
+}
