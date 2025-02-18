a7891b715afb3e0d4dd640c17249d43ffbecf216
Kleyson Tavares <kleyson.tavares@icloud.com>
2025-02-17 23:08:38 -0300

    adiciona aba de settings e refatora nomes de variaveis

--- ImageIA/Coordinator/TabBarCoordinator.swift
+++ ImageIA/Coordinator/TabBarCoordinator.swift
@@ -11,8 +11,10 @@ class TabBarCoordinator: Coordinator {
     var navigationController: UINavigationController
     var tabBarController: UITabBarController
     var homeCoordinator: HomeCoordinator?
-    var searchNavigationController: UINavigationController? // 🔹 Guarda referência da aba Search
+    var imageNavigationController: UINavigationController? // 🔹 Guarda referência da aba Search
+    var settingsNavigationController: UINavigationController? // 🔹 Guarda referência da aba Settings
     var imageViewController: ImageViewController? // 🔹 Guarda referência do ImageViewController
+    var settingsViewController: SettingsViewController? // 🔹 Guarda referência do SettingsViewController
 
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
@@ -24,15 +26,20 @@ class TabBarCoordinator: Coordinator {
         homeCoordinator?.start()
 
         guard let homeNavigationController = homeCoordinator?.navigationController else { return }
-        homeNavigationController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 0)
+        homeNavigationController.tabBarItem = UITabBarItem(title: "Criar", image: UIImage(systemName: "house"), tag: 0)
 
         let imageVC = ImageViewController()
         self.imageViewController = imageVC // 🔹 Armazena referência para passar a imagem depois
-        searchNavigationController = UINavigationController(rootViewController: imageVC)
-        searchNavigationController?.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        imageNavigationController = UINavigationController(rootViewController: imageVC)
+        imageNavigationController?.tabBarItem = UITabBarItem(title: "Minhas Imagens", image: UIImage(systemName: "photo.on.rectangle"), tag: 1)
 
-        tabBarController.viewControllers = [homeNavigationController, searchNavigationController!]
+        let settingsVC = SettingsViewController()
+        self.settingsViewController = settingsVC // 🔹 Armazena referência para passar a imagem depois
+        settingsNavigationController = UINavigationController(rootViewController: settingsVC)
+        settingsNavigationController?.tabBarItem = UITabBarItem(title: "Ajustes", image: UIImage(systemName: "gear"), tag: 1)
 
+        guard let imageNav = imageNavigationController, let settingsNav = settingsNavigationController else { return }
+        tabBarController.viewControllers = [homeNavigationController, imageNav, settingsNav]
         tabBarController.tabBar.tintColor = .purple  // Cor do ícone ativo
         tabBarController.tabBar.unselectedItemTintColor = .gray  // Cor dos ícones inativos
         tabBarController.tabBar.backgroundColor = .white  // Cor de fundo da TabBar
